91bec116db3058cac3ebc6736c28dff1b44eb298
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-03-10 12:09:29 +0000

    changed font size and added a list of text

--- script.js
+++ script.js
@@ -4,3 +4,5 @@ document.getElementById("start").onclick = function() {
   window.location.href = "intro.html";
 
 };
+
+const dreamTxt = ["You won a lottery allowing you to board the fabled Boohbah express before it opens to the public", "You're first day here was exciting and tired you out", "But you won't remain asleep  for long.."]
