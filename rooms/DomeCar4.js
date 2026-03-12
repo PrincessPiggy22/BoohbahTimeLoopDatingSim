8dfe43c164d33b97e814c9dd9511d1822d7063f5
PrincessPiggy22 <mlpuryear@my.waketech.edu>
2026-03-12 12:41:21 +0000

    phil zingzing collins

---
+++ rooms/DomeCar4.js
@@ -0,0 +1,51 @@
+
+function goThruDialouge(id, array, characters) {
+
+  let index = 0;
+  document.getElementById(id).innerHTML = array[0];
+  document.getElementById("characterImage").src = characters[0];
+
+  document.addEventListener("click", function() {
+  index++;
+
+  if(index < array.length) {
+
+    document.getElementById(id).innerHTML = array[index];
+    document.getElementById("characterImage").src = characters[index];
+    
+  }
+  if(index == array.length){
+    if (index == array.length) {
+  setTimeout(() => {
+    window.location.href = "PhilZingZingCollins.html";
+  }, 2000);
+}
+  }
+});
+};
+
+
+const inventory = ["key"];
+
+Bling = "../sprites/Blingbah.png";
+Jing = "../sprites/Jingbah.png";
+Zing = "../sprites/Zingbah.png";
+ZingDead = "../sprites/ZingbahDead.png";
+ZingPhil = "../sprites/Philzingzingcollins.png";
+ZingBorax = "../sprites/ZingbahBorax.png";
+Hum = "../sprites/Humbah.png";
+Jum = "../sprites/Jumbah.png";
+Zum = "../sprites/Zumbah.png";
+You = "../sprites/you.png"; 
+Lorax = "../sprites/Lorax.png";
+
+
+// Begning convo
+const character1 =[Zing,Zing,You,ZingPhil];
+
+const dialouge1 = ["...","&quot;I don't care anymore&quot;","&quot;my bad&quot;","&quot;I don't care anymOoOoRe~&quot;"];
+console.log("ZingZing time!!!!");
+goThruDialouge("roomText", dialouge1, character1);
+
+
+
